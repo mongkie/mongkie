8f0143f31b43bedfd0ef875007360a4b73cabd9b
Yeongjun Jang <yjjang@kribb.re.kr>
2013-01-09 13:23:20 +0900

    Only if visual style setting tab is selected and OK button clicked, unchanged visual styles would be applied

--- InteractionManagerUI/src/org/mongkie/ui/im/SettingsPanel.java
+++ InteractionManagerUI/src/org/mongkie/ui/im/SettingsPanel.java
@@ -79,7 +79,7 @@ public class SettingsPanel extends javax.swing.JPanel {
             VisualStyle<EdgeItem> modelEdgeStyle =
                     Lookup.getDefault().lookup(InteractionController.class).getEdgeVisualStyle(is).reset(edgeStyleUI.getVisualStyle());
             // Then, apply the style to the visual items
-            if (!edgeStyleUI.isApplied()) {
+            if (!edgeStyleUI.isApplied() && tabbedPane.getTitleAt(tabbedPane.getSelectedIndex()).equals("Visual Styles")) {
                 modelEdgeStyle.apply(edgeStyleUI.getVisualItems());
             }
         } else if (edgeStyleUI.isApplied()) { // If canceled but some styles are applied
